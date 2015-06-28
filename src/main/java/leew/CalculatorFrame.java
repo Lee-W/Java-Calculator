2e80f69f892b669a1d990a6f4b82153919926bfc
LeeW <cl87654321@gmail.com>
2015-06-28 22:12:01 +0800

    Rename and rearrange code in CalculatorFrame and HistoryPanel

--- src/main/java/leew/CalculatorFrame.java
+++ src/main/java/leew/CalculatorFrame.java
@@ -7,70 +7,76 @@ import java.awt.*;
  * Created by LeeW on 6/27/15.
  */
 public class CalculatorFrame extends JFrame {
+    private static final String TITLE = "Calculator";
+
     private JLabel memoryIndicator = new JLabel();
     private JTextField resultField = new JTextField(24);
-    
     private HistoryPanel historyPanel = new HistoryPanel();
     private OperationPanel operationPanel = new OperationPanel(this);
-    
 
-    public static void main (String[] args) {
-        CalculatorFrame frame = new CalculatorFrame();
-        frame.setupPanels();
-        frame.setVisible(true);
-    }
-    
     public CalculatorFrame() {
-        setTitle("Calculator");
+        setTitle(TITLE);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridBagLayout());
+
+        createUIComponents();
+        setupPanels();
     }
-    
-    public void setupPanels() {
+
+    public static void main(String[] args) {
+        CalculatorFrame frame = new CalculatorFrame();
+        frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
         memoryIndicator.setText("M");
         memoryIndicator.setVisible(false);
-        
-        GridBagConstraints c3 = new GridBagConstraints();
-        c3.gridx = 0;
-        c3.gridy = 0;
-        c3.gridheight = 1;
-        c3.gridwidth = 1;
-        this.add(memoryIndicator, c3);
-        
+
         resultField.setText(" ");
         resultField.setSize(100, 100);
         resultField.setEditable(false);
-        GridBagConstraints c0 = new GridBagConstraints();
-        c0.gridx = 1;
-        c0.gridy = 0;
-        c0.gridwidth = 4;
-        c0.gridheight = 1;
-        this.add(resultField, c0);
+    }
+
+    private void setupPanels() {
+        GridBagConstraints indicatorPos = new GridBagConstraints();
+        indicatorPos.gridx = 0;
+        indicatorPos.gridy = 0;
+        indicatorPos.gridheight = 1;
+        indicatorPos.gridwidth = 1;
+        this.add(memoryIndicator, indicatorPos);
+
+        GridBagConstraints resultPos = new GridBagConstraints();
+        resultPos.gridx = 1;
+        resultPos.gridy = 0;
+        resultPos.gridwidth = 4;
+        resultPos.gridheight = 1;
+        this.add(resultField, resultPos);
+
+        GridBagConstraints operationPos = new GridBagConstraints();
+        operationPos.gridx = 0;
+        operationPos.gridy = 1;
+        operationPos.gridheight = 6;
+        operationPos.gridwidth = 5;
+        this.add(operationPanel, operationPos);
+
+        GridBagConstraints historyPos = new GridBagConstraints();
+        historyPos.gridx = 6;
+        historyPos.gridy = 0;
+        historyPos.gridheight = 8;
+        historyPos.gridwidth = 6;
+        this.add(historyPanel, historyPos);
         
-        GridBagConstraints c1 = new GridBagConstraints();
-        c1.gridx = 0;
-        c1.gridy = 1;
-        c1.gridheight = 6;
-        c1.gridwidth = 5;
-        this.add(operationPanel, c1);
-        
-        GridBagConstraints c2 = new GridBagConstraints();
-        c2.gridx = 6;
-        c2.gridy = 0;
-        c2.gridheight = 8;
-        c2.gridwidth = 6;
-        this.add(historyPanel, c2);
         this.pack();
     }
-    
+
     public void updateEquation(String eq) {
         resultField.setText(eq);
     }
-    
+
     public void addHistory(String eq) {
         historyPanel.addHistory(eq);
     }
-    
+
     public void setMemoryStatus(boolean status) {
         memoryIndicator.setVisible(status);
     }
--- src/main/java/leew/HistoryPanel.java
+++ src/main/java/leew/HistoryPanel.java
@@ -9,91 +9,88 @@ import java.net.URL;
  */
 public class HistoryPanel extends JPanel {
     private static final URL PIC_PATH = ClassLoader.getSystemClassLoader().getResource("img/sauce.jpg");
-    
+
     private String historyData = "";
-    private boolean historyIsEnabled = true; 
-    
-    private JScrollPane jScrollPane = new JScrollPane();
-    
-    private JTextArea displayArea = new JTextArea(10, 30);
-    private JLabel picLabel = new JLabel();
-    
+    private boolean historyIsEnabled = true;
+
     private JButton showPictureButton = new JButton();
     private JButton showHistoryButton = new JButton();
     private JButton clearHistoryButton = new JButton();
-    
+
+    private JScrollPane jScrollPane = new JScrollPane();
+    private JTextArea historyArea = new JTextArea(10, 30);
+    private JLabel picLabel = new JLabel();
+
     public HistoryPanel() {
         this.setLayout(new GridBagLayout());
         createUIComponents();
-    }
-    
-    public void addHistory(String equation) {
-        historyData += equation + "\n";
-        if (historyIsEnabled) {
-            displayArea.setText(historyData);
-        }
+        addComponents();
     }
     
     private void createUIComponents() {
         showPictureButton.setText("Show Picture");
-        showPictureButton.addActionListener(actionListener -> {
+        showPictureButton.addActionListener(listener -> {
             jScrollPane.setViewportView(picLabel);
             historyIsEnabled = false;
         });
 
         showHistoryButton.setText("Show History");
-        showHistoryButton.addActionListener(actionListener -> {
-            displayArea.setText(historyData);
-            jScrollPane.setViewportView(displayArea);
+        showHistoryButton.addActionListener(listener -> {
+            historyArea.setText(historyData);
+            jScrollPane.setViewportView(historyArea);
             historyIsEnabled = true;
         });
 
         clearHistoryButton.setText("Clear History");
         clearHistoryButton.addActionListener(actionListener -> {
             historyData = "";
-            displayArea.setText(historyData);
+            historyArea.setText(historyData);
         });
-        
-        ImageIcon imageIcon =new ImageIcon(PIC_PATH);
+
+        ImageIcon imageIcon = new ImageIcon(PIC_PATH);
         picLabel.setIcon(imageIcon);
         picLabel.setBounds(100, 100, 100, 100);
 
-        displayArea.setBounds(100, 100, 100, 100);
-        displayArea.setEnabled(false);
+        historyArea.setBounds(100, 100, 100, 100);
+        historyArea.setEnabled(false);
 
-        jScrollPane.setViewportView(displayArea);
+        jScrollPane.setViewportView(historyArea);
         jScrollPane.setBounds(100, 100, 100, 100);
-        
-        addComponents();
     }
-    
+
     private void addComponents() {
-        GridBagConstraints c0 = new GridBagConstraints();
-        c0.gridx = 0;
-        c0.gridy = 0;
-        c0.gridwidth = 1;
-        c0.gridheight = 1;
-        this.add(showPictureButton, c0);
-        
-        GridBagConstraints c1 = new GridBagConstraints();
-        c1.gridx = 1;
-        c1.gridy = 0;
-        c1.gridwidth = 1;
-        c1.gridheight = 1;
-        this.add(showHistoryButton, c1);
-        
-        GridBagConstraints c2 = new GridBagConstraints();
-        c2.gridx = 2;
-        c2.gridy = 0;
-        c2.gridwidth = 1;
-        c2.gridheight = 1;
-        this.add(clearHistoryButton, c2);
-       
-        GridBagConstraints c3 = new GridBagConstraints();
-        c3.gridx = 0;
-        c3.gridy = 1;
-        c3.gridwidth = 3;
-        c3.gridheight = 6;
-        this.add(jScrollPane, c3);
+        GridBagConstraints showPicBtnPos = new GridBagConstraints();
+        showPicBtnPos.gridx = 0;
+        showPicBtnPos.gridy = 0;
+        showPicBtnPos.gridwidth = 1;
+        showPicBtnPos.gridheight = 1;
+        this.add(showPictureButton, showPicBtnPos);
+
+        GridBagConstraints showHistoryBtnPos = new GridBagConstraints();
+        showHistoryBtnPos.gridx = 1;
+        showHistoryBtnPos.gridy = 0;
+        showHistoryBtnPos.gridwidth = 1;
+        showHistoryBtnPos.gridheight = 1;
+        this.add(showHistoryButton, showHistoryBtnPos);
+
+        GridBagConstraints clearHistoryBtnPos = new GridBagConstraints();
+        clearHistoryBtnPos.gridx = 2;
+        clearHistoryBtnPos.gridy = 0;
+        clearHistoryBtnPos.gridwidth = 1;
+        clearHistoryBtnPos.gridheight = 1;
+        this.add(clearHistoryButton, clearHistoryBtnPos);
+
+        GridBagConstraints scrollPanePos = new GridBagConstraints();
+        scrollPanePos.gridx = 0;
+        scrollPanePos.gridy = 1;
+        scrollPanePos.gridwidth = 3;
+        scrollPanePos.gridheight = 6;
+        this.add(jScrollPane, scrollPanePos);
+    }
+
+    public void addHistory(String equation) {
+        historyData += equation + "\n";
+        if (historyIsEnabled)
+            historyArea.setText(historyData);
     }
 }
